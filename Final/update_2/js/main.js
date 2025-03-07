f022a4462da260ede3b848ae126cf40f4108d10e
scross621 <scross0621@gmail.com>
2025-03-06 19:24:30 -0500

    Add content to pages

---
+++ Final/update_2/js/main.js
@@ -0,0 +1,64 @@
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+const navbarStyles = window.getComputedStyle(navbar);
+
+const droplink = document.querySelector("#nav-droplink");
+const dropdown = document.querySelector("#nav-dropdown");
+const dropStyles = window.getComputedStyle(dropdown);
+
+let click = 0;
+
+// Stop transitions from running when browser is resized
+let resizeTimer = null;
+window.addEventListener("resize", () => {
+  document.body.classList.add("stop-transition");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("stop-transition");
+  }, 100);
+})
+
+document.addEventListener("click", () => { 
+if(dropdown.classList.contains('visible') && document.activeElement !== droplink) {
+  dropdown.classList.toggle('visible')
+  dropdown.classList.remove('height')
+}
+})
+
+droplink.addEventListener("click", () => {
+  if (navbarStyles.getPropertyValue('position') === 'fixed') {
+    if (dropStyles.getPropertyValue('height') == '0px'){
+      dropdown.classList.toggle('visible')
+      dropdown.classList.toggle('height')
+      
+    }else {
+      dropdown.classList.toggle('height')
+      dropdown.classList.toggle('visible')
+    }
+  } else {
+    dropdown.classList.toggle('visible')
+  }
+})
+
+hamburger.addEventListener("click", () => {
+  if (click === 0){
+    
+    hamburger.classList.toggle('active');
+    navbar.style.right = 0;
+    navbar.style.visibility = 'visible';
+    click = 1;
+    
+  }
+  else {
+    navbar.style.right = '-100%';
+    navbar.style.visibility = null;
+    hamburger.classList.toggle('active');
+    click = 0;
+  
+  if (dropStyles.getPropertyValue('height') !== '0px'){
+    dropdown.style.height = null;
+    dropdown.style.opacity = null;
+  }
+}
+  
+})
